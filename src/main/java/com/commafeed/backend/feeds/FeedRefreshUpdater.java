e5667dc26f36c19e2808ce90e2268d12785b72e0
Athou <jeremiepanzer@gmail.com>
2013-07-30 17:03:03 +0200

    wraps all feed entries update in a single commit

--- src/main/java/com/commafeed/backend/feeds/FeedRefreshUpdater.java
+++ src/main/java/com/commafeed/backend/feeds/FeedRefreshUpdater.java
@@ -1,20 +1,14 @@
 package com.commafeed.backend.feeds;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
-import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang3.time.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,13 +22,11 @@ import com.commafeed.backend.feeds.FeedRefreshExecutor.Task;
 import com.commafeed.backend.model.ApplicationSettings;
 import com.commafeed.backend.model.Feed;
 import com.commafeed.backend.model.FeedEntry;
-import com.commafeed.backend.model.FeedEntryContent;
 import com.commafeed.backend.model.FeedSubscription;
 import com.commafeed.backend.pubsubhubbub.SubscriptionHandler;
 import com.commafeed.backend.services.ApplicationSettingsService;
 import com.commafeed.backend.services.FeedUpdateService;
 import com.google.api.client.util.Lists;
-import com.google.common.util.concurrent.Striped;
 
 @ApplicationScoped
 public class FeedRefreshUpdater {
@@ -69,14 +61,12 @@ public class FeedRefreshUpdater {
 	CacheService cache;
 
 	private FeedRefreshExecutor pool;
-	private Striped<Lock> locks;
 
 	@PostConstruct
 	public void init() {
 		ApplicationSettings settings = applicationSettingsService.get();
 		int threads = Math.max(settings.getDatabaseUpdateThreads(), 1);
 		pool = new FeedRefreshExecutor("feed-refresh-updater", threads, Math.min(50 * threads, 1000));
-		locks = Striped.lazyWeakLock(threads * 100000);
 	}
 
 	@PreDestroy
@@ -101,28 +91,33 @@ public class FeedRefreshUpdater {
 		@Override
 		public void run() {
 			boolean ok = true;
-			if (entries.isEmpty() == false) {
+			if (!entries.isEmpty()) {
 
-				List<String> lastEntries = cache.getLastEntries(feed);
-				List<String> currentEntries = Lists.newArrayList();
-
-				List<FeedSubscription> subscriptions = null;
+				List<String> cachedEntries = cache.getLastEntries(feed);
+				List<String> newCachedEntries = Lists.newArrayList();
+				List<FeedEntry> newEntries = Lists.newArrayList();
 				for (FeedEntry entry : entries) {
 					String cacheKey = cache.buildUniqueEntryKey(feed, entry);
-					if (!lastEntries.contains(cacheKey)) {
+					if (!cachedEntries.contains(cacheKey)) {
 						log.debug("cache miss for {}", entry.getUrl());
-						if (subscriptions == null) {
-							subscriptions = feedSubscriptionDAO.findByFeed(feed);
-						}
-						ok &= addEntry(feed, entry, subscriptions);
+						newEntries.add(entry);
 						metricsBean.entryCacheMiss();
 					} else {
 						log.debug("cache hit for {}", entry.getUrl());
 						metricsBean.entryCacheHit();
 					}
-					currentEntries.add(cacheKey);
+					newCachedEntries.add(cacheKey);
 				}
-				cache.setLastEntries(feed, currentEntries);
+				if (!newEntries.isEmpty()) {
+					List<FeedSubscription> subscriptions = feedSubscriptionDAO.findByFeed(feed);
+					try {
+						ok = feedUpdateService.addEntries(feed, newEntries, subscriptions);
+					} catch (Exception e) {
+						log.error(e.getMessage(), e);
+						ok = false;
+					}
+				}
+				cache.setLastEntries(feed, newCachedEntries);
 			}
 
 			if (applicationSettingsService.get().isPubsubhubbub()) {
@@ -140,45 +135,6 @@ public class FeedRefreshUpdater {
 		public boolean isUrgent() {
 			return feed.isUrgent();
 		}
-	}
-
-	private boolean addEntry(final Feed feed, final FeedEntry entry, final List<FeedSubscription> subscriptions) {
-		boolean success = false;
-
-		// lock on feed, make sure we are not updating the same feed twice at
-		// the same time
-		String key1 = StringUtils.trimToEmpty("" + feed.getId());
-
-		// lock on content, make sure we are not updating the same entry
-		// twice at the same time
-		FeedEntryContent content = entry.getContent();
-		String key2 = DigestUtils.sha1Hex(StringUtils.trimToEmpty(content.getContent() + content.getTitle()));
-
-		Iterator<Lock> iterator = locks.bulkGet(Arrays.asList(key1, key2)).iterator();
-		Lock lock1 = iterator.next();
-		Lock lock2 = iterator.next();
-		boolean locked1 = false;
-		boolean locked2 = false;
-		try {
-			locked1 = lock1.tryLock(1, TimeUnit.MINUTES);
-			locked2 = lock2.tryLock(1, TimeUnit.MINUTES);
-			if (locked1 && locked2) {
-				feedUpdateService.updateEntry(feed, entry, subscriptions);
-				success = true;
-			} else {
-				log.error("lock timeout for " + feed.getUrl() + " - " + key1);
-			}
-		} catch (InterruptedException e) {
-			log.error("interrupted while waiting for lock for " + feed.getUrl() + " : " + e.getMessage(), e);
-		} finally {
-			if (locked1) {
-				lock1.unlock();
-			}
-			if (locked2) {
-				lock2.unlock();
-			}
-		}
-		return success;
 	}
 
 	private void handlePubSub(final Feed feed) {
--- src/main/java/com/commafeed/backend/services/FeedUpdateService.java
+++ src/main/java/com/commafeed/backend/services/FeedUpdateService.java
@@ -2,6 +2,8 @@ package com.commafeed.backend.services;
 
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
@@ -9,21 +11,30 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.commafeed.backend.MetricsBean;
 import com.commafeed.backend.cache.CacheService;
 import com.commafeed.backend.dao.FeedEntryDAO;
 import com.commafeed.backend.dao.FeedEntryStatusDAO;
 import com.commafeed.backend.dao.FeedSubscriptionDAO;
+import com.commafeed.backend.feeds.FeedRefreshUpdater;
 import com.commafeed.backend.model.Feed;
 import com.commafeed.backend.model.FeedEntry;
 import com.commafeed.backend.model.FeedEntryContent;
 import com.commafeed.backend.model.FeedSubscription;
 import com.commafeed.backend.model.User;
 import com.google.common.collect.Lists;
+import com.google.common.util.concurrent.Striped;
 
 @Stateless
 public class FeedUpdateService {
+
+	protected static Logger log = LoggerFactory.getLogger(FeedRefreshUpdater.class);
+
+	private static Striped<Lock> LOCKS = Striped.lazyWeakLock(100000);
 
 	@PersistenceContext
 	protected EntityManager em;
@@ -46,10 +57,58 @@ public class FeedUpdateService {
 	@Inject
 	FeedEntryContentService feedEntryContentService;
 
+	public boolean addEntries(final Feed feed, final List<FeedEntry> entries, final List<FeedSubscription> subscriptions) {
+		List<String> keys = Lists.newArrayList();
+		for (FeedEntry entry : entries) {
+			// lock on feed, make sure we are not updating the same feed twice at the same time
+			keys.add(StringUtils.trimToEmpty("" + feed.getId()));
+
+			// lock on content, make sure we are not updating the same entry twice at the same time
+			FeedEntryContent content = entry.getContent();
+			keys.add(DigestUtils.sha1Hex(StringUtils.trimToEmpty(content.getContent() + content.getTitle())));
+		}
+
+		List<Lock> locks = Lists.newArrayList();
+		boolean locked = true;
+		try {
+			for (Lock lock : LOCKS.bulkGet(keys)) {
+				if (lock.tryLock(1, TimeUnit.MINUTES)) {
+					locks.add(lock);
+				} else {
+					locked = false;
+					break;
+				}
+			}
+
+			if (locked) {
+				for (FeedEntry entry : entries) {
+					addEntry(feed, entry);
+				}
+			} else {
+				log.error("lock timeout for {}", feed.getUrl());
+			}
+		} catch (InterruptedException e) {
+			log.error("interrupted while waiting for lock for " + feed.getUrl() + " : " + e.getMessage(), e);
+		} finally {
+			for (Lock lock : locks) {
+				lock.unlock();
+			}
+		}
+
+		List<User> users = Lists.newArrayList();
+		for (FeedSubscription sub : subscriptions) {
+			users.add(sub.getUser());
+		}
+		cache.invalidateUnreadCount(subscriptions.toArray(new FeedSubscription[0]));
+		cache.invalidateUserRootCategory(users.toArray(new User[0]));
+
+		return locked;
+	}
+
 	/**
-	 * this is NOT thread-safe
+	 * thread-unsafe insertion
 	 */
-	public void updateEntry(Feed feed, FeedEntry entry, List<FeedSubscription> subscriptions) {
+	private void addEntry(Feed feed, FeedEntry entry) {
 
 		Long existing = feedEntryDAO.findExisting(entry.getGuid(), feed.getId());
 		if (existing != null) {
@@ -62,14 +121,7 @@ public class FeedUpdateService {
 		entry.setInserted(new Date());
 		entry.setFeed(feed);
 
-		List<User> users = Lists.newArrayList();
-		for (FeedSubscription sub : subscriptions) {
-			User user = sub.getUser();
-			users.add(user);
-		}
 		feedEntryDAO.saveOrUpdate(entry);
 		metricsBean.entryInserted();
-		cache.invalidateUnreadCount(subscriptions.toArray(new FeedSubscription[0]));
-		cache.invalidateUserRootCategory(users.toArray(new User[0]));
 	}
 }
